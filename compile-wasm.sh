fc6f76d65b67ca9b9133964296caddb5ff533026
Todd Eischeid <toddeischeid@gmail.com>
2019-08-19 16:28:01 -0400

    getting compile options to work correctly for using in angular

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -13,11 +13,14 @@
 #  /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.html mt19937-64.c prng-utils-mt19937.c -O3 -s WASM=1  -s EXPORT_NAME='PrngEnscriptenModule' -s NO_EXIT_RUNTIME=1 -s MALLOC='dlmalloc' -s FILESYSTEM=0 -s MODULARIZE=1 -s EXPORT_ES6=1 -s ENVIRONMENT='web' -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
 
 
+# NOTE: for settings options see: https://github.com/emscripten-core/emscripten/blob/incoming/src/settings.js
 
 # use default js only generation
 # It seems -O3 optimization may accidentally slice _malloc() from generated code, so we use 02 here
-  /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.js mt19937-64.c prng-utils-mt19937.c -O2 -s WASM=1  -s NO_EXIT_RUNTIME=1 -s MALLOC='dlmalloc' -s FILESYSTEM=0 -s MODULARIZE=1  -s EMIT_EMSCRIPTEN_METADATA=1 -s EXPORT_ES6=1 -s ENVIRONMENT='web' -s FILESYSTEM=0 -s MODULARIZE=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
+  /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.js mt19937-64.c prng-utils-mt19937.c -O2 -s WASM=1  -s NO_EXIT_RUNTIME=1 -s MALLOC='dlmalloc' -s FILESYSTEM=0 -s MODULARIZE=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s EXPORT_ES6=1 -s ENVIRONMENT='web' -s FILESYSTEM=0  -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
 
+
+# -s EXPORTED_FUNCTIONS="['_processData', '_malloc']".
 
 
  # /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.wasm mt19937-64.c prng-utils-mt19937.c -O3 -s -s WASM=1 -s NO_EXIT_RUNTIME=1  -s EMIT_EMSCRIPTEN_METADATA=1

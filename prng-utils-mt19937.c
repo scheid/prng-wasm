555dfeebf4eb15f56d4daf214ac65cbc36d29cda
Todd Eischeid <toddeischeid@gmail.com>
2019-08-20 08:44:57 -0400

    debug the random weights function

--- prng-utils-mt19937.c
+++ prng-utils-mt19937.c
@@ -307,6 +307,12 @@ int32_t* EMSCRIPTEN_KEEPALIVE chooseRandomItemWeighted(double* weights, int coun
     int32_t* result; //[valueCount];
     int i;
 
+    printf("in EMSCRIPTEN_KEEPALIVE chooseRandomItemWeighted C function\n");
+
+    for (i = 0; i < count; i++) {
+        printf("weights %i: %f\n", i, weights[i]);
+    }
+
     result = malloc(valueCount * sizeof(int32_t));
 
     for (i = 0; i < valueCount; i++) {

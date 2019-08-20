c53827adffe49540d15d5bb290563700f620dc6b
Todd Eischeid <toddeischeid@gmail.com>
2019-08-19 21:01:09 -0400

    experiment with compile options

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -17,10 +17,10 @@
 
 # use default js only generation
 # It seems -O3 optimization may accidentally slice _malloc() from generated code, so we use 02 here
-  /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.js mt19937-64.c prng-utils-mt19937.c -O2 -s WASM=1  -s NO_EXIT_RUNTIME=1 -s MALLOC='dlmalloc' -s FILESYSTEM=0 -s MODULARIZE=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s EXPORT_ES6=1 -s ENVIRONMENT='web' -s FILESYSTEM=0  -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
+  /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.js mt19937-64.c prng-utils-mt19937.c -O2 -s WASM=1  -s NO_EXIT_RUNTIME=1 -s MALLOC='dlmalloc' -s FILESYSTEM=0 -s MODULARIZE=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s EXPORT_ES6=1 -s ENVIRONMENT='web' -s FILESYSTEM=0 -s EXPORTED_FUNCTIONS="['_main', '_malloc']"  -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
 
 
-# -s EXPORTED_FUNCTIONS="['_processData', '_malloc']".
+# -s EXPORTED_FUNCTIONS="['_main', '_malloc']".
 
 
  # /home/eischeid/web-assembly/emsdk/fastcomp/emscripten/emcc.py -o prng-wasm.wasm mt19937-64.c prng-utils-mt19937.c -O3 -s -s WASM=1 -s NO_EXIT_RUNTIME=1  -s EMIT_EMSCRIPTEN_METADATA=1

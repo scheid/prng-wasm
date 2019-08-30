e11438c8acb1978450a5b77b218f13440f13489e
scheid <toddeischeid@gmail.com>
2019-08-29 20:52:57 -0400

    file cleanup

--- wasm-to-bytes.js
+++ wasm-to-bytes.js
@@ -17,7 +17,7 @@ var outFile = inFile.replace(".wasm", "") + "-byte-array.js";
 var binary = fs.readFileSync(inFile);
 var binaryBytesStr = binary.join(",");  // will be decimal bytes
 
-fs.writeFile(outFile, "// Inline this byte array directly into your javascript source, and then compile the bytes as normal, a la:\n// WebAssembly.instantiate(wasmBytes, yourImportObject)).then(obj => { obj.instance.exports.exported_func();  });\n\nlet wasmBytes = [" + binaryBytesStr + "];", (err) => {
+fs.writeFile(outFile, "// Inline this byte array directly into your javascript source, and then compile the bytes as normal, a la:\n// WebAssembly.instantiate(wasmBytes, yourImportObject)).then(obj => { obj.instance.exports.exported_func();  });\n\nvar PrngWasmByteArray = [" + binaryBytesStr + "];\n\nexport default PrngWasmByteArray;", (err) => {
     if (err) { console.log("  - ERROR: file write error", err); } else { console.log("  - conversion complete: written to: " + outFile + "\n"); }
 })
 
